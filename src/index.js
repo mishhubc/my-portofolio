c8211d384cce3f54bf326ef9a6befd5d991a4904
MihaiJ <mihaijicum@gmail.com>
2021-02-10 20:59:43 +0100

    footer year

--- src/index.js
+++ src/index.js
@@ -17,6 +17,10 @@ $('a[href^="#"]').on('click', function(event) {
       );
   }
 });
+const footer.Text=document.querySelector('footer__text');
+const year=new Date().getFullYear();
+footer.innerHTML=`© ${year} <a href="https://github.com/mishhubc"></a> and <a href="https://github.com/covidev" target="_blank"></a>  
+</a>`
 
 initSr();
 initTilt();
